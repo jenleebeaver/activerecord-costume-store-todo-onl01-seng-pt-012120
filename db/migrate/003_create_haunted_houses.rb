6b8649d5615f08f61fc04e2578c1be85212c6bf9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-01 19:10:09 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,9 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :location
       t.string :theme
       t.integer :price
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
+      t.boolean :family_friendly
       t.datetime :opening_time
       t.datetime :closing_time
 
