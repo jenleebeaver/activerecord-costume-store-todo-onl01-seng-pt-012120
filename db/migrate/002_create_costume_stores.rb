c5f161eba63d650ee65f20bc2c588b68fd0c2b84
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-01 19:07:38 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,6 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
+      t.integer :num_of_employees
       t.string :size
       t.string :image_url
       t.float :price
