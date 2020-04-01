1315c693320f05d7c0de7c523fd1bc97490d8e1f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-01 19:06:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
+    end
+  end
+end
