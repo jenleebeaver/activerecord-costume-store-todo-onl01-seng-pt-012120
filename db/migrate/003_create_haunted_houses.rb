e622097a221caf184b06ee37002562832742d130
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-01 19:11:19 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.string :theme
-      t.float :price
+      t.float :price #float includes $ decimal
       t.boolean :family_friendly
       t.datetime :opening_time
       t.datetime :closing_time
